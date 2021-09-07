570102d92841a804892bad6a4d3f64dde12655d9
Mbali <u18027734@tuks.co.za>
2021-09-07 08:34:25 +0200

    add-campus-and-date

--- 6.4.0/aspnet-core/src/VillageBoard.EntityFrameworkCore/EntityFrameworkCore/VillageBoardDbContext.cs
+++ 6.4.0/aspnet-core/src/VillageBoard.EntityFrameworkCore/EntityFrameworkCore/VillageBoardDbContext.cs
@@ -4,13 +4,16 @@ using VillageBoard.Authorization.Roles;
 using VillageBoard.Authorization.Users;
 using VillageBoard.MultiTenancy;
 using VillageBoard.Announcement_Types;
+using VillageBoard.Campuses;
 
 namespace VillageBoard.EntityFrameworkCore
 {
     public class VillageBoardDbContext : AbpZeroDbContext<Tenant, Role, User, VillageBoardDbContext>
     {
         public DbSet<Announcement_Type> Announcement_Types { get; set; }
-        
+        public DbSet<Campus> Campuses { get; set; }
+
+
         public VillageBoardDbContext(DbContextOptions<VillageBoardDbContext> options)
             : base(options)
         {
