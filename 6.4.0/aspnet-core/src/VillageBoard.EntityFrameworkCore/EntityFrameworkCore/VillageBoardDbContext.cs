19b8b2883f8695862b7372f87a77188d50cd95fa
Mbali <u18027734@tuks.co.za>
2021-09-07 05:08:18 +0200

    Base APB Framework

---
+++ 6.4.0/aspnet-core/src/VillageBoard.EntityFrameworkCore/EntityFrameworkCore/VillageBoardDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using VillageBoard.Authorization.Roles;
+using VillageBoard.Authorization.Users;
+using VillageBoard.MultiTenancy;
+
+namespace VillageBoard.EntityFrameworkCore
+{
+    public class VillageBoardDbContext : AbpZeroDbContext<Tenant, Role, User, VillageBoardDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public VillageBoardDbContext(DbContextOptions<VillageBoardDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
