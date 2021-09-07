3365b545f6089f90e79f35e1957ff9ee016586f6
Mbali <u18027734@tuks.co.za>
2021-09-07 10:56:54 +0200

    add-room-inspection

---
+++ 6.4.0/aspnet-core/src/VillageBoard.Core/Room Inspections/Room Inspection.cs
@@ -0,0 +1,80 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillageBoard.Room_Inspections
+{
+    public class Room_Inspection : Entity<int>
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Curtains { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Curtain_Rail_Hooks { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Globes { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Light_Switches { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Desk { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Chair { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Wardrobe_Doors { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Mattress { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Wall_Sockets { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Room_Door { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Door_Lock { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Mirror { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Ceiling { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Walls { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public DateTime Floor { get; set; }
+
+        
+        [MaxLength(225)]
+        public DateTime Other { get; set; }
+    }
+}
