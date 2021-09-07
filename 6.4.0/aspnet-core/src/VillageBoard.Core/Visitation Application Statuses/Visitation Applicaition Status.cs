6c6a7fa34cf08b0a7f4f88cf8b9abd176129138c
Mbali <u18027734@tuks.co.za>
2021-09-07 09:40:03 +0200

    add-visitation-application-status

---
+++ 6.4.0/aspnet-core/src/VillageBoard.Core/Visitation Application Statuses/Visitation Applicaition Status.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillageBoard.Visitation_Application_Statuses
+{
+    public class Visitation_Applicaition_Status : Entity<int>
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Decsription { get; set; }
+
+    }
+}
