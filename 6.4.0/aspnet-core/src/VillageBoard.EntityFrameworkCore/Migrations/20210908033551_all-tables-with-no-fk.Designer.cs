425700e751d68defe9db31b45a314aa7dc5d94f4
Mbali <u18027734@tuks.co.za>
2021-09-08 05:54:02 +0200

    Some fixes on tables generation

--- 6.4.0/aspnet-core/src/VillageBoard.EntityFrameworkCore/Migrations/20210908033551_all-tables-with-no-fk.Designer.cs
+++ 6.4.0/aspnet-core/src/VillageBoard.EntityFrameworkCore/Migrations/20210908033551_all-tables-with-no-fk.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VillageBoard.EntityFrameworkCore;
 
 namespace VillageBoard.Migrations
 {
     [DbContext(typeof(VillageBoardDbContext))]
-    partial class VillageBoardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210908033551_all-tables-with-no-fk")]
+    partial class alltableswithnofk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
